57695620f24b4e3056804fcad0b94163ddfb3c28
samuel <damosisamuel@gmail.com>
2020-12-14 15:34:27 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+var eta = parseInt(prompt("inserisci la tua eta"));
+
+var km = parseInt(prompt("inserisci il km"));
+
+
+var kmPrice = km * 0.21;
+
+console.log("km price " + kmPrice);
+
+var minEta;
+var maxEta;
+
+if (eta < 18){
+  minEta=(kmPrice * 20) / 100;
+} else {
+  minEta = kmPrice
+}
+
+// console.log("Teen Price offer" + minEta);
+
+if (eta > 65) {
+  maxEta = (kmPrice * 40) / 100;
+}
+else{
+  maxEta = kmPrice
+}
+
+console.log("teen price offer " + minEta + " adult price offer " + maxEta);
